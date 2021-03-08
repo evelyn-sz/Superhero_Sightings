41550f6f131e5ffed3397e2efd1c444c50556c7f
Richard Bodsworth <richardbodsworth@gmail.com>
2021-03-08 14:47:07 +0000

    merge develop to feature

---
+++ superhero_sightings_front/src/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const NavBar = () => {
+    return(
+        <header>
+
+            <ul>
+                <li>
+                    <a href="/sightings">Home</a>
+                </li>
+                <li>
+                    <a href="/sightings/new">Create sighting</a>
+                </li>
+
+
+            </ul>
+
+
+
+        </header>
+    )
+}
+
+export default NavBar;
---
+++ superhero_sightings_front/src/containers/MainContainer.js
@@ -0,0 +1,22 @@
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import NavBar from '../NavBar';
+import SightingsContainer from './SightingsContainer';
+
+
+
+const MainContainer = () =>{
+    return(
+        <Router>
+        <Fragment>
+            <NavBar/>
+            <Switch>
+                <Route path="/sightings" component={SightingsContainer}/>
+            </Switch>
+        </Fragment>
+        </Router>
+
+    )
+}
+
+export default MainContainer;
