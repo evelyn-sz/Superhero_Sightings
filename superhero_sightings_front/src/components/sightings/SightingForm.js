e28f29681dd0822c4e5c9a33b23c8dcd8dfd8ef9
Bela <belahamid@gmail.com>
2021-03-08 22:50:46 +0000

    refactored sighting form heavily, and edited sightings container

--- superhero_sightings_front/src/components/sightings/SightingForm.js
+++ superhero_sightings_front/src/components/sightings/SightingForm.js
@@ -1,69 +1,111 @@
-import React, {useState}  from 'react';
+import React, {Fragment, useState}  from 'react';
 
 
-const SightingForm = ({onSightingSubmit, onCreate}) => {
+const SightingForm = ({props}) => {
 
-    const [superhero, setSuperhero] = useState("");			
-    const [city, setCity] = useState(""); 
-    const [date, setDate] = useState(""); 
+  const [stateSighting, setStateSighting] = useState();
+
+    // const [superhero, setSuperhero] = useState("");			
+    // const [location, setLocation] = useState(""); 
+    // const [date, setDate] = useState(""); 
 
 
-const handleSuperheroChange = (evt) => {			
-    setSuperhero(evt.target.value);
+// const handleSuperheroChange = (evt) => {			
+//     setSuperhero(evt.target.value);
+//   }
+
+//   const handleLocationChange = (evt) => {			
+//     setLocation(evt.target.value);
+//   }
+
+//   const handleDateChange = (evt) => {			
+//     setDate(evt.target.value);
+//   }
+  const [sighting, setSighting] = useState(0);
+
+  const findLocationIndex =() =>{
+    if (props.sighting) {
+      return props.locations.findIndex(location => {props.sighting.location.id === location.id})
+    } else {
+      return null;
+    }
   }
 
-  const handleCityChange = (evt) => {			
-    setCity(evt.target.value);
+  const handleChange = (event) => {
+    let propertyName = event.target.name;
+    let sighting = props.sighting
+    sighting[propertyName] = event.target.value;
+    setSighting({sighting:sighting})
+
   }
 
-  const handleDateChange = (evt) => {			
-    setDate(evt.target.value);
+  const handleLocation = (event) =>{
+    const index = parseInt(event.target.value)
+    const selectedLocation = locations[index]
+    sighting["location"] = selectedLocation
+    setSighting({sighting:sighting})
+    
   }
 
   const handleFormSubmit = (evt) => {
     evt.preventDefault();
     const superheroToSubmit = superhero.trim();
-    const cityToSubmit = city.trim();
+    const locationToSubmit = location.trim();
     const dateToSubmit = date.trim();
-    if (!superheroToSubmit || !cityToSubmit){
+    if (!superheroToSubmit || !locationToSubmit){
       return
     }
     onSightingSubmit({						
         superhero: superheroToSubmit,				
-        city: cityToSubmit, 
+        location: locationToSubmit, 
         date: dateToSubmit		
       });
 
     setSuperhero("");  
-    setCity("");    
+    setLocation("");    
     setDate("");    
 
   }
 
     return (
-        <form className="sighting-form" onSubmit={handleFormSubmit}>
-        <input
-          type="text"
-          placeholder="Superhero name"
-          value={superhero} 
-          onChange={handleSuperheroChange}									
-        />
-        <input
-          type="text"
-          placeholder="City"
-          value={city} 		
-          onChange={handleCityChange}							
-        />
-         <input
-          type="date"
-          placeholder="Date"
-          value={date} 		
-          onChange={handleDateChange}							
-        />
-        <input type="submit" value="Submit" />
-      </form>
+      <Fragment>
+        <h3>Create sighting</h3>
+        <form onSubmit={handleFormSubmit}>
+          <input type="text" placeholder="time" name="time" onChange={handleChange} value={props.sighting.time}/>
+          <input type="text" placeholder="date" name="date" onChange={handleChange} value={props.sighting.date}/>
+          <select name="location" onChange={handleLocation} defaultValue={findLocationIndex()|| "select-location"}>
+          <option disabled value="select-location" > select a location</option>
+          {locationOptions}
+          </select>
+          <button type="submit">Save</button>
+        </form>
+      </Fragment>
+      
     );
 }
 
 
 export default SightingForm;
+
+
+{/* <form className="sighting-form" onSubmit={handleFormSubmit}>
+      //   <input */}
+      //     type="text"
+      //     placeholder="Superhero name"
+      //     value={superhero} 
+      //     onChange={handleSuperheroChange}									
+      //   />
+      //   <input
+      //     type="text"
+      //     placeholder="Location"
+      //     value={location} 		
+      //     onChange={handleLocationChange}							
+      //   />
+      //    <input
+      //     type="date"
+      //     placeholder="Date"
+      //     value={date} 		
+      //     onChange={handleDateChange}							
+      //   />
+      //   <input type="submit" value="Submit" />
+      // </form>
--- superhero_sightings_front/src/containers/SightingsContainer.js
+++ superhero_sightings_front/src/containers/SightingsContainer.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import SightingForm from '../components/sightings/SightingForm';
 import SightingList from '../components/sightings/SightingList';
+import Request from '../helpers/request';
 
 
 
@@ -24,42 +25,55 @@ const SightingsContainer = () => {
         ]
     )
 
+    const [superheroes, setSuperheroes] = useState([])
+    const [locations, setLocations] = useState([])
+
     const addSighting = (submittedSighting) => {
         submittedSighting.id = Date.now();
         const updatedSightings = [...sightings, submittedSighting];
         setSightings(updatedSightings);
       }
 
+    useEffect(() => {getAllData()}, [sightings])
+
     const getAllData = () =>{
         console.log("getting data...");
         const request = new Request();
         const sightingPromise = request.get('/api/sightings')
+        const superheroPromise = request.get('/api/superheroes')
+        const locationPromise = request.get('/api/locations')
 
-        Promise.all([sightingPromise])
+        Promise.all([sightingPromise, superheroPromise, locationPromise])
         .then((data => {
-            setSightings();
+            setSightings(data[0]);
+            setLocations(data[1]);
+            setSuperheroes(data[2])
         }))
 
+    }
+    const handlePost = (sighting) => {
+        const request = new Request();
+        request.post("/api/sightings", sighting)
+        .then(() => window.location = '/sightings')
+    }
 
-        const handlePost = (sighting) => {
-            const request = new Request();
-            request.post("/api/sightings", sighting)
-            .then(() => window.location = '/sightings')
-        }
 
-        if (!{sightings}) {
-            return null
-        }
-
+    if (!{sightings}) {
+        return null
     }
     
 
     return (
+        <Router>
         <>
 
         <Switch>
             <Route exact path = '/sightings/new' render={
-                () => {return <SightingForm onSightingSubmit={(sighting) => addSighting(sighting)} onCreate={handlePost}/> }
+                (props) => {return <SightingForm 
+                    superheroes={superheroes}
+                    locations={locations}
+                    onSightingSubmit={(sighting) => addSighting(sighting)} 
+                    onCreate={handlePost}/> }
             }/>
         </Switch>
 
@@ -69,6 +83,7 @@ const SightingsContainer = () => {
         <h2>Report a Sighting</h2>
         	
         </>
+        </Router>
 
     );
 }
