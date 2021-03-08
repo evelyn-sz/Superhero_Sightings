0b3dae9fa6b1866904477fcd5c7f5a625ceb14e9
Bela <belahamid@gmail.com>
2021-03-08 01:53:51 +0000

    created GET request for /sightings

--- src/main/java/com/codeclan/example/SuperheroSightings/controllers/SightingController.java
+++ src/main/java/com/codeclan/example/SuperheroSightings/controllers/SightingController.java
@@ -1,4 +1,32 @@
 package com.codeclan.example.SuperheroSightings.controllers;
 
+import com.codeclan.example.SuperheroSightings.models.Sighting;
+import com.codeclan.example.SuperheroSightings.repositories.SightingRepository;
+import com.codeclan.example.SuperheroSightings.repositories.SuperheroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class SightingController {
+    @Autowired
+    SightingRepository sightingRepository;
+
+    // Show all sightings: GET '/sightings'
+    @GetMapping(value = "/sightings")
+    public ResponseEntity<List<Sighting>> getAllSightings(){
+        return new ResponseEntity<>(sightingRepository.findAll(), HttpStatus.OK);
+    }
+    // Show sighting by id: GET '/sightings/{id}'
+    @GetMapping(value="/sightings/{id}")
+    public ResponseEntity
+    // Create : POST '/sightings'
+    // Edit: PATCH '/sightings/{id}'
+    // Delete: DELETE '/sightings/{id}'
+
 }
+
