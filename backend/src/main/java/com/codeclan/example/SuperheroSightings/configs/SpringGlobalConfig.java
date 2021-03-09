52f2316967116815680dea48d42d603a1d578bdd
evelyn-sz <evelynszarek@gmail.com>
2021-03-09 14:39:39 +0000

    Added config route in backend

---
+++ backend/src/main/java/com/codeclan/example/SuperheroSightings/configs/SpringGlobalConfig.java
@@ -0,0 +1,2 @@
+package com.codeclan.example.SuperheroSightings.configs;public class SpringGlobalConfig {
+}
--- superhero_sightings_front/src/containers/SightingsContainer.js
+++ superhero_sightings_front/src/containers/SightingsContainer.js
@@ -1,56 +1,9 @@
-import React from 'react';
-import SightingForm from '../components/sightings/SightingForm';
-
-
-const SightingsContainer = () => {
-
-    const[sightings, setSightings] = useState (
-        [
-            {
-                id: 1,
-                superhero: "Batman", 
-                city: "Rome", 
-                date: "27-02-2020"
-            },
-            {
-                id: 2, 
-                superhero: "Superman",
-                city: "Melbourne",
-                date: "22-04-2021"
-            }
-        ]
-    )
-
-    const addSighting = (submittedSighting) => {
-        submittedSighting.id = Date.now();
-        const updatedSightings = [...sightings, submittedSighting];
-        setSightings(updatedSightings);
-      }
-    
-
-    return (
-        <>
-        <h1>Superhero Sightings</h1>
-        <h2>Recent Sightings</h2>
-        <SightingList sightings ={sightings} />	
-        <h2>Report a Sighting</h2>
-        <SightingForm onSightingSubmit={(sighting) => addSighting(sighting)}/>	
-        </>
-
-    );
-}
-
-
-export default SightingsContainer;
-
 import React, {useEffect, useState} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import SightingForm from '../components/sightings/SightingForm';
 import SightingList from '../components/sightings/SightingList';
 import SightingDetail from '../components/sightings/SightingDetail';
 import Request from '../helpers/request';
-
-
 
 const SightingsContainer = () => {
 
