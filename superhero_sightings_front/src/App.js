2edbcb9ff61e64ef88cba8f82556049cba73771e
Bela <belahamid@gmail.com>
2021-03-10 14:00:59 +0000

    markers appear in correct region of map

--- superhero_sightings_front/src/App.js
+++ superhero_sightings_front/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import MainContainer from './containers/MainContainer'
 import MapContainer from './containers/MapContainer'
+import 'leaflet/dist/leaflet.css';
 // import SightingsContainer from "./containers/SightingsContainer"
 
 
