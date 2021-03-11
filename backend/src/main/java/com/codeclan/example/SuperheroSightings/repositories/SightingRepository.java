58d7dbe9d2ab75cc4c8774631548d119e5ac5cd1
evelyn-sz <evelynszarek@gmail.com>
2021-03-11 11:41:30 +0000

    Sightings filtered by superhero or id

--- backend/src/main/java/com/codeclan/example/SuperheroSightings/repositories/SightingRepository.java
+++ backend/src/main/java/com/codeclan/example/SuperheroSightings/repositories/SightingRepository.java
@@ -4,6 +4,10 @@ import com.codeclan.example.SuperheroSightings.models.Sighting;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SightingRepository extends JpaRepository<Sighting, Long> {
+    public List<Sighting> findAllBySuperheroId(Long superheroId);
+    public List<Sighting> findAllByLocationId(Long locationId);
 }
--- backend/src/test/java/com/codeclan/example/SuperheroSightings/SuperheroSightingsApplicationTests.java
+++ backend/src/test/java/com/codeclan/example/SuperheroSightings/SuperheroSightingsApplicationTests.java
@@ -1,15 +1,31 @@
 package com.codeclan.example.SuperheroSightings;
 
 import com.codeclan.example.SuperheroSightings.models.Location;
+import com.codeclan.example.SuperheroSightings.models.Sighting;
 import com.codeclan.example.SuperheroSightings.models.Superhero;
+import com.codeclan.example.SuperheroSightings.repositories.LocationRepository;
+import com.codeclan.example.SuperheroSightings.repositories.SightingRepository;
+import com.codeclan.example.SuperheroSightings.repositories.SuperheroRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class SuperheroSightingsApplicationTests {
+	@Autowired
+	SightingRepository sightingRepository;
+
+	@Autowired
+	SuperheroRepository superheroRepository;
+
+	@Autowired
+	LocationRepository locationRepository;
+
 
 	@Test
 	void contextLoads() {
@@ -23,4 +39,18 @@ class SuperheroSightingsApplicationTests {
 		assertEquals("Superman", superman.getName());
 		assertEquals("Edinburgh", location.getName());
 	}
+
+	@Test
+	public void canFindSightingsOfSpecificSuperhero(){
+		List<Sighting> found = sightingRepository.findAllBySuperheroId(3L);
+		assertEquals(2, found.size());
+	}
+
+	@Test
+	public void canFindSightingsOfSpecificLocation(){
+		List<Sighting> found = sightingRepository.findAllByLocationId(3L);
+		assertEquals(3, found.size());
+	}
+
+
 }
